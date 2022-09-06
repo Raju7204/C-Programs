477ee3ad6237457ce9da6bcd229502b9b686e7ac
Raju7204 <112969092+Raju7204@users.noreply.github.com>
2022-09-06 21:59:47 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,n,flag=0;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    for(i=2;i<=(n/2);i++)
+    {
+        if(n%i==0)
+        {
+            printf("It is not a prime number\n");
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0)
+    {
+        printf("It is a prime number\n");
+    }
+return 0;
+}
