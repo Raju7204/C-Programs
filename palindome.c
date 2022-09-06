c1b6d1da8b4b8b245a922066f6853c30729193eb
Raju7204 <112969092+Raju7204@users.noreply.github.com>
2022-09-06 22:04:41 +0530

    Add files via upload
    //Checking weather the given string is palindrome or not

---
+++ palindome.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[30];
+    int i,flag=0;
+    printf("Enter the palindrome :\n");
+    gets(str);
+    for(i=0;i<strlen(str);i++)
+    {
+        if(str[i]==str[strlen(str)-i-1])
+        {
+            flag=1;
+        }
+        else
+        {
+            flag=0;
+        }
+    }
+    if(flag==1)
+    {
+        printf("It is a palindrome");
+    }
+    else
+    {
+        printf("It is not palindrome");
+    }
+return 0;
+}
