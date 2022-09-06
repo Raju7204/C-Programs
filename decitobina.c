3b0dd760741e84634b1f201da778d7e01fd07623
Raju7204 <112969092+Raju7204@users.noreply.github.com>
2022-09-06 22:02:00 +0530

    Add files via upload

---
+++ decitobina.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n,i,a[30];
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    for(i=0;n>0;i++)
+    {
+        a[i]=n%2;
+        n=n/2;
+    }
+    printf("The binary number of the given number is :\n");
+    for(i=i-1;i>=0;i--)
+    {
+        printf("%d",a[i]);
+    }
+return 0;
+}
